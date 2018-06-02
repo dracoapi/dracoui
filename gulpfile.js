3996675b5f36f130bb2b8e6574ccfd66a7024ddd
niicojs <niicojs@outlook.fr>
2018-06-02 11:13:03 +0200

    switch to gulp 4

--- gulpfile.js
+++ gulpfile.js
@@ -20,19 +20,24 @@ gulp.task('dev-styles', function() {
 });
 
 gulp.task('watch-styles', function() {
-   return gulp.watch('src/assets/css/*.scss', ['dev-styles']);
+   return gulp.watch('src/assets/css/*.scss', gulp.parallel('dev-styles'));
 });
 
-gulp.task('watch', [ 'dev-styles', 'watch-styles' ]);
+gulp.task('watch', gulp.series('dev-styles', 'watch-styles' ));
 
 // Build
 
-gulp.task('styles', ['static'], function() {
+gulp.task('static', function() {
+    return gulp.src(['src/**/*', '!src/scripts/**/*', '!src/assets/css/**/*', '!src/index.html'])
+               .pipe(gulp.dest('./build'));
+});
+
+gulp.task('styles', gulp.series('static', function() {
     return gulp.src('src/assets/css/*.scss')
                 .pipe(sass().on('error', sass.logError))
                 .pipe(cssnano())
                 .pipe(gulp.dest('build/assets/css'));
-});
+}));
 
 gulp.task('scripts', function() {
     return gulp.src('src/index.html')
@@ -49,28 +54,23 @@ gulp.task('scripts', function() {
                 .pipe(gulp.dest('./build'));
 });
 
-gulp.task('static', ['clean'], function() {
-  return gulp.src(['src/**/*', '!src/scripts/**/*', '!src/assets/css/**/*', '!src/index.html'])
-             .pipe(gulp.dest('./build'));
-});
-
 gulp.task("clean", function() {
    return del("build/*"); 
 });
 
-gulp.task('build', [ 'clean', 'static', 'styles', 'scripts' ]);
+gulp.task('build',  gulp.series('clean', gulp.parallel('static', 'styles', 'scripts' )));
 
 // Deploy
 
-gulp.task('deploy', ['build'], function() {
+gulp.task('deploy', gulp.series('build', function() {
   return gulp.src([
       './build/**/*'
     ])
     .pipe(file('CNAME', "ui.dracoapi.ml"))
     .pipe(gulp.dest('./build'))
     .pipe(ghPages({remoteUrl: "https://github.com/dracoapi/dracoui.git"}));
-});
+}));
 
 // Default
 
-gulp.task('default', [ 'dev-styles', 'build' ]);
+gulp.task('default', gulp.series('dev-styles', 'build' ));
